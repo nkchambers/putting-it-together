68074d509576ef6c6751d0766b56e9d5178de0a2
nkchambers <nkchames@gmail.com>
2022-07-14 19:50:56 -0700

    adding putting-it-together for 7-14-22

---
+++ putting_it_together/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import PersonCardComponent from './components/PersonCardComponent';
+
+function App() {
+  console.log("hello react");
+
+  /*const person1 = {
+    firstName: "Jane",s
+    lastName: "Doe",
+    age: 45,
+    hairColor: "black"
+  }*/
+
+
+  return (
+    <fieldset>
+      <legend>App.js</legend>
+      <div className="App">
+        
+        <hr />
+          <PersonCardComponent firstName={ "Jane" } lastName={ "Doe" } age={ 45 } hairColor = { "Black" }/>
+          <PersonCardComponent firstName={ "John" } lastName={ "Smith" } age={ 88 } hairColor = { "Brown" }/>
+          <PersonCardComponent firstName={ "Millard" } lastName={ "Fillmore" } age={ 50 } hairColor = { "Brown" }/>
+          <PersonCardComponent firstName={ "Maria" } lastName={ "Smith" } age={ 62 } hairColor = { "Brown" }/>
+      </div>
+    </fieldset>
+  );
+}
+
+export default App;
